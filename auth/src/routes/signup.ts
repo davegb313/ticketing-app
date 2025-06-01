c35300d1812c5fe10205fdcdfccb0a987cc2fcc6
David Besnovatiy <davidbesnovatiy@gmail.com>
2025-06-01 15:51:02 +0300

    implementing jwt token creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
 import { RequstValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -26,6 +27,15 @@ router.post('/api/users/signup', [
 
     const user = User.build({ email, password });
     await user.save();
+
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+        jwt: userJwt
+    };
 
     res.status(201).send(user);
 });
