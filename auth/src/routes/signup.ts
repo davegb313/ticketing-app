2cfed53950ac5fa063ad2c074ec7ed8d7ab8e60a
David Besnovatiy <davidbesnovatiy@gmail.com>
2025-06-03 12:30:03 +0300

    adding authorizing validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,22 +1,16 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
-import { RequstValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middleware/validate-request';
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('paassword must be between 4 and 20 characters')
-], async (req: Request, res: any) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        throw new RequstValidationError(errors.array());
-    }
-
+], validateRequest, async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
