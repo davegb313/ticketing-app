6878e66a8158c146c52998fe050f70da45e1da08
David Besnovatiy <davidbesnovatiy@gmail.com>
2025-06-05 20:39:23 +0300

    handaling reqeusts from ssr to services

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,23 @@
-const LandingPage = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+
     return <h1>landing1 page</h1>
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+
+    if (typeof window === 'undefined') {
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+            headers: req.headers
+        });
+        console.log(data);
+        return data;
+    } else {
+        const { data } = await axios.get('/api/users/currentuser');
+        return data;
+    }
+}
 
 export default LandingPage;
