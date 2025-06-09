028978c15ea8206d88b07eb4d6a043aa62b37fc3
David Besnovatiy <davidbesnovatiy@gmail.com>
2025-06-09 11:30:04 +0300

    exporting common functionality to the outside package

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,8 +7,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { NotFoundError } from './errors/not-found-error';
-import { errorHandler } from './middleware/error-handler';
+import { NotFoundError, errorHandler } from '@daviegb/ticketing-common';
 
 const app = express();
 app.set('trust proxy', true);
