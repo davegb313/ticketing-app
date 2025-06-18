2f3b92295ea61f359041d1cb29ed0df803d7e3c5
David Besnovatiy <davidbesnovatiy@gmail.com>
2025-06-18 12:11:54 +0300

    nats server implementation

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,15 @@
+import { Message } from 'node-nats-streaming';
+import { Listener } from '../../../../common/src/events/base-listener';
+import { Subjects } from '../../../../common/src/events/subjects';
+import { TicketCreatedEvent } from '../../../../common/src/events/ticket-created-event';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    readonly subject = Subjects.TicketCreated;
+    queueGroupName = 'tickets -service';
+
+    onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+        console.log('event data', data);
+
+        msg.ack();
+    }
+}
