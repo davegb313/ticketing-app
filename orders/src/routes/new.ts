dfe57b18936ebb8e0fffdc6657f4dd9656a6d83a
David Besnovatiy <davidbesnovatiy@gmail.com>
2025-06-21 23:46:50 +0300

    oreders service starting func

---
+++ orders/src/routes/new.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@daviegb/ticketing-common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+
+const router = express.Router();
+
+router.post('/api/orders', requireAuth, [
+    body('ticketId')
+        .not()
+        .isEmpty()
+        .custom((input: string) => { mongoose.Types.ObjectId.isValid(input) })
+        .withMessage('ticketId must be provided')
+], validateRequest, async (req: Request, res: Response) => {
+    res.send({});
+});
+
+export { router as newOrderRouter }
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -74,6 +74,5 @@ it('publishes an event', async () => {
     })
     .expect(201);
 
-    expect(natsWrapper.client.publish).toHaveBeenCalled();
-    
-  });
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
